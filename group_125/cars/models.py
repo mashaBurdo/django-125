73f7e4c71f968445c21be35545933faaa607134d
Denis <Johnyferrari94@gmail.com>
2022-04-25 16:51:02 +0300

    models car

--- group_125/cars/models.py
+++ group_125/cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    title = models.CharField(max_length=40) # название машинок
+    content = models.TextField()
+    costs = models.IntegerField()
+    create = models.IntegerField(default=2010)
+
+    def __str__(self):
+        return self.title
